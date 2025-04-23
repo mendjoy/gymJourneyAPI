1b5cb18bcd24d9489e98ff4b1c4cc27ccb957b3a
mendjoy <joycemendes.jm79@gmail.com>
2025-04-23 19:44:14 -0300

    ajustes repositorio

--- src/main/java/io/github/mendjoy/gymJourneyAPI/repository/WorkoutRepository.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/repository/WorkoutRepository.java
@@ -3,6 +3,6 @@ package io.github.mendjoy.gymJourneyAPI.repository;
 import io.github.mendjoy.gymJourneyAPI.entity.workout.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WorkoutRepository extends JpaRepository<Integer, Workout> {
+public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
 
 }
