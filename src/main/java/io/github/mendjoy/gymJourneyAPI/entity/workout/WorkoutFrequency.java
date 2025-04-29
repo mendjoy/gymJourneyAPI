0e9b8237bad4ef3b03914c5300a9471c5d222ae7
mendjoy <joycemendes.jm79@gmail.com>
2025-04-28 21:55:58 -0300

    entidade de frequencia de treino

--- src/main/java/io/github/mendjoy/gymJourneyAPI/entity/workout/WorkoutFrequency.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/entity/workout/WorkoutFrequency.java
@@ -1,4 +1,67 @@
 package io.github.mendjoy.gymJourneyAPI.entity.workout;
 
+import io.github.mendjoy.gymJourneyAPI.entity.user.User;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "workout_frequency")
 public class WorkoutFrequency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_id")
+    private Workout workout;
+
+    @Column(name = "frequency_date")
+    private Date frequencyDate;
+
+    public WorkoutFrequency() {
+    }
+
+    public WorkoutFrequency(User user, Workout workout, Date frequencyDate) {
+        this.user = user;
+        this.workout = workout;
+        this.frequencyDate = frequencyDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Workout getWorkout() {
+        return workout;
+    }
+
+    public void setWorkout(Workout workout) {
+        this.workout = workout;
+    }
+
+    public Date getFrequencyDate() {
+        return frequencyDate;
+    }
+
+    public void setFrequencyDate(Date frequencyDate) {
+        this.frequencyDate = frequencyDate;
+    }
 }
