ad82c6b5b524dac914942903b9e92609dade3344
mendjoy <joycemendes.jm79@gmail.com>
2025-04-29 20:40:08 -0300

    implementando classe base do dto

--- src/main/java/io/github/mendjoy/gymJourneyAPI/dto/workout/WorkoutDTO.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/dto/workout/WorkoutDTO.java
@@ -3,67 +3,15 @@ package io.github.mendjoy.gymJourneyAPI.dto.workout;
 
 import java.util.List;
 
-public class WorkoutDTO {
-    private Integer id;
-    private Integer userId;
-    private String name;
-    private String description;
-    private Integer maxSessions;
+public class WorkoutDTO extends WorkoutBaseDTO {
+
     private List<WorkoutExerciseDTO> exercises;
 
     public WorkoutDTO() {
     }
 
     public WorkoutDTO(Integer id, Integer userId, String name, String description, Integer maxSessions, List<WorkoutExerciseDTO> exercises) {
-        this.id = id;
-        this.userId = userId;
-        this.name = name;
-        this.description = description;
-        this.maxSessions = maxSessions;
-        this.exercises = exercises;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getMaxSessions() {
-        return maxSessions;
-    }
-
-    public void setMaxSessions(Integer maxSessions) {
-        this.maxSessions = maxSessions;
-    }
-
-    public void setExercises(List<WorkoutExerciseDTO> exercises) {
+        super();
         this.exercises = exercises;
     }
 
@@ -71,7 +19,7 @@ public class WorkoutDTO {
         return exercises;
     }
 
-    public void setExercise(List<WorkoutExerciseDTO> exercises) {
+    public void setExercises(List<WorkoutExerciseDTO> exercises) {
         this.exercises = exercises;
     }
 }
--- src/main/java/io/github/mendjoy/gymJourneyAPI/dto/workout/WorkoutDetailsDTO.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/dto/workout/WorkoutDetailsDTO.java
@@ -1,4 +1,24 @@
 package io.github.mendjoy.gymJourneyAPI.dto.workout;
 
-public class WorkoutDetailsDTO {
+import java.util.List;
+
+public class WorkoutDetailsDTO extends WorkoutBaseDTO {
+
+    private List<WorkoutExerciseDetailsDTO> exercises;
+
+    public WorkoutDetailsDTO() {
+    }
+
+    public WorkoutDetailsDTO(Integer id, Integer userId, String name, String description, Integer maxSessions, List<WorkoutExerciseDetailsDTO> exercises) {
+        super();
+        this.exercises = exercises;
+    }
+
+    public List<WorkoutExerciseDetailsDTO> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<WorkoutExerciseDetailsDTO> exercises) {
+        this.exercises = exercises;
+    }
 }
