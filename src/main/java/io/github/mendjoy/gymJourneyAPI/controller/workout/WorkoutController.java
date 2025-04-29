1970e3126c053984261f62b09052377bda3f572e
mendjoy <joycemendes.jm79@gmail.com>
2025-04-29 19:10:14 -0300

    ajustes anotations

--- src/main/java/io/github/mendjoy/gymJourneyAPI/controller/workout/WorkoutController.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/controller/workout/WorkoutController.java
@@ -6,11 +6,10 @@ import io.github.mendjoy.gymJourneyAPI.service.WorkoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/workout")
+@RestController
+@RequestMapping("/workout")
 public class WorkoutController {
 
     @Autowired
