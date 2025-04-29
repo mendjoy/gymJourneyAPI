33133424a1421507906227706329dcd019aacfe9
mendjoy <joycemendes.jm79@gmail.com>
2025-04-29 20:39:08 -0300

    novas classes

---
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/dto/workout/WorkoutBaseDTO.java
@@ -0,0 +1,60 @@
+package io.github.mendjoy.gymJourneyAPI.dto.workout;
+
+public class WorkouBaseDTO {
+    private Integer id;
+    private Integer userId;
+    private String name;
+    private String description;
+    private Integer maxSessions;
+
+    public WorkouBaseDTO() {
+    }
+
+    public WorkouBaseDTO(Integer id, Integer userId, String name, String description, Integer maxSessions) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.description = description;
+        this.maxSessions = maxSessions;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getMaxSessions() {
+        return maxSessions;
+    }
+
+    public void setMaxSessions(Integer maxSessions) {
+        this.maxSessions = maxSessions;
+    }
+}
---
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/dto/workout/WorkoutExerciseDetailsDTO.java
@@ -0,0 +1,4 @@
+package io.github.mendjoy.gymJourneyAPI.dto.workout;
+
+public class WorkoutExerciseDetailsDTO {
+}
