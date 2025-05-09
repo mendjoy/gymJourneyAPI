fd2aa92d950cc7b3b5adec80bc6184d24554016a
mendjoy <joycemendes.jm79@gmail.com>
2025-05-08 21:22:32 -0300

    service para salvar workout e entidades relacionadas

--- src/main/java/io/github/mendjoy/gymJourneyAPI/service/WorkoutService.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/service/WorkoutService.java
@@ -1,13 +1,16 @@
 package io.github.mendjoy.gymJourneyAPI.service;
 
 import io.github.mendjoy.gymJourneyAPI.dto.exercise.ExerciseDTO;
-import io.github.mendjoy.gymJourneyAPI.dto.workout.WorkoutDTO;
-import io.github.mendjoy.gymJourneyAPI.dto.workout.WorkoutDetailsDTO;
-import io.github.mendjoy.gymJourneyAPI.dto.workout.WorkoutExerciseDetailsDTO;
+import io.github.mendjoy.gymJourneyAPI.dto.workout.*;
+import io.github.mendjoy.gymJourneyAPI.dto.workout.request.WorkoutRequestDTO;
+import io.github.mendjoy.gymJourneyAPI.dto.workout.response.WorkoutExerciseResponseDTO;
+import io.github.mendjoy.gymJourneyAPI.dto.workout.response.WorkoutResponseDTO;
+import io.github.mendjoy.gymJourneyAPI.dto.workout.response.WorkoutSectionResponseDTO;
 import io.github.mendjoy.gymJourneyAPI.entity.exercise.Exercise;
 import io.github.mendjoy.gymJourneyAPI.entity.user.User;
 import io.github.mendjoy.gymJourneyAPI.entity.workout.Workout;
 import io.github.mendjoy.gymJourneyAPI.entity.workout.WorkoutExercise;
+import io.github.mendjoy.gymJourneyAPI.entity.workout.WorkoutSection;
 import io.github.mendjoy.gymJourneyAPI.repository.ExerciseRepository;
 import io.github.mendjoy.gymJourneyAPI.repository.UserRepository;
 import io.github.mendjoy.gymJourneyAPI.repository.WorkoutRepository;
@@ -30,48 +33,59 @@ public class WorkoutService {
     @Autowired
     WorkoutRepository workoutRepository;
 
-    public WorkoutDetailsDTO register(WorkoutDTO workoutDTO){
+    public WorkoutDTO register(WorkoutRequestDTO workoutRequestDTO){
 
-        User user = userRepository.findById(workoutDTO.getUserId()).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado!"));
+        User user = userRepository.findById(workoutRequestDTO.getUserId()).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado!"));
 
-        Workout workout = new Workout(user, workoutDTO.getName(), workoutDTO.getDescription(), workoutDTO.getMaxSessions());
+        Workout workout = new Workout(user, workoutRequestDTO.getName(), workoutRequestDTO.getDescription(), workoutRequestDTO.getMaxSessions());
 
-        List<WorkoutExercise> workoutExercises = workoutDTO.getExercises().stream().map(ex -> {
-            Exercise exercise = exerciseRepository.findById(ex.getExerciseId())
-                                                  .orElseThrow(() -> new EntityNotFoundException("Exercício: " + ex.getExerciseId() + " não encontrado!"));
-            return new WorkoutExercise(workout, exercise, ex.getSets(), ex.getRepetitions(),
-                    ex.getWeight(), ex.getRestTime());
+        List<WorkoutSection> workoutSections = workoutRequestDTO.getWorkoutSection().stream().map(sec -> {
+
+            WorkoutSection section = new WorkoutSection(workout, sec.getName(), sec.getDescription());
+
+            List<WorkoutExercise> workoutExercises = sec.getWorkoutExercise().stream().map(ex -> {
+                Exercise exercise = exerciseRepository.findById(ex.getId()).orElseThrow(() -> new EntityNotFoundException("Exercício: " + ex.getId() + " não encontrado!"));
+                return new WorkoutExercise(section, exercise, ex.getSets(), ex.getRepetitions(), ex.getWeight(), ex.getRestTime());
+            }).toList();
+
+            section.setWorkoutExercises(workoutExercises);
+            return section;
         }).toList();
 
-         workout.setWorkoutExercises(workoutExercises);
+         workout.setWorkoutSections(workoutSections);
 
          Workout newWorkout = workoutRepository.save(workout);
 
          return getWorkoutDetailsDTO(newWorkout);
-
     }
 
-    private WorkoutDetailsDTO getWorkoutDetailsDTO(Workout workout){
-        List<WorkoutExerciseDetailsDTO> workoutExerciseDetailsDTOS = workout.getWorkoutExercises().stream().map(we -> {
-            Exercise exercise = exerciseRepository.findById(we.getExercise().getId()).orElseThrow(() -> new EntityNotFoundException("Exercício não encontrado!"));
-            ExerciseDTO exerciseDTO = new ExerciseDTO(exercise.getId(),
-                                                      exercise.getName(),
-                                                      exercise.getDescription(),
-                                                      exercise.getMuscle_group());
+    private WorkoutResponseDTO getWorkoutDetailsDTO(Workout workout){
 
-            return new WorkoutExerciseDetailsDTO(we.getId(),
-                                                 exerciseDTO,
-                                                 we.getSets(),
-                                                 we.getRepetitions(),
-                                                 we.getWeight(),
-                                                 we.getRestTime());
+        List<WorkoutSectionResponseDTO> workoutSectionResponseDTO = workout.getWorkoutSections().stream().map( ws -> {
+
+            List<WorkoutExerciseResponseDTO> workoutExerciseResponseDTOS = ws.getWorkoutExercises().stream().map(we -> {
+                Exercise exercise = exerciseRepository.findById(we.getExercise().getId()).orElseThrow(() -> new EntityNotFoundException("Exercício não encontrado!"));
+                ExerciseDTO exerciseDTO = new ExerciseDTO(exercise.getId(),
+                                                          exercise.getName(),
+                                                          exercise.getDescription(),
+                                                          exercise.getMuscle_group());
+
+                return new WorkoutExerciseResponseDTO(we.getId(),
+                                                      we.getSets(),
+                                                      we.getRepetitions(),
+                                                      we.getWeight(),
+                                                      we.getRestTime(),exerciseDTO);
+            }).toList();
+
+            return new WorkoutSectionResponseDTO(ws.getId(), ws.getName(), ws.getDescription(), workoutExerciseResponseDTOS);
         }).toList();
 
-        return new WorkoutDetailsDTO(workout.getId(),
-                                     workout.getUser().getId(),
-                                     workout.getName(),
-                                     workout.getDescription(),
-                                     workout.getMaxSessions(),
-                                     workoutExerciseDetailsDTOS);
+        return new WorkoutResponseDTO(workout.getId(),
+                                      workout.getUser().getId(),
+                                      workout.getName(),
+                                      workout.getDescription(),
+                                      workout.getMaxSessions(),
+                                      workoutSectionResponseDTO);
+
     }
 }
