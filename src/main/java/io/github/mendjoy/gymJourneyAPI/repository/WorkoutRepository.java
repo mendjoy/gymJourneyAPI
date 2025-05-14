7ef5cea6b8e8a29c8b034fb0d072d82f65291bb3
mendjoy <joycemendes.jm79@gmail.com>
2025-05-14 15:19:28 -0300

    adicionando query para buscar o treino atual do usuario com a data nula ou com a data de termino maior que a data atual

--- src/main/java/io/github/mendjoy/gymJourneyAPI/repository/WorkoutRepository.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/repository/WorkoutRepository.java
@@ -3,6 +3,8 @@ package io.github.mendjoy.gymJourneyAPI.repository;
 import io.github.mendjoy.gymJourneyAPI.entity.user.User;
 import io.github.mendjoy.gymJourneyAPI.entity.workout.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,6 +12,7 @@ public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
 
     List<Workout> findByUser(User user);
 
-    Workout findByUserAndEndDateIsNull(User user);
+    @Query("SELECT w FROM Workout w WHERE w.user = :user AND (w.endDate IS NULL OR w.endDate >= CURRENT_DATE)")
+    Workout findCurrentWorkout(@Param("user") User user);
 
 }
