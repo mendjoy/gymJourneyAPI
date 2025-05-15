dbd41edd019652ceb6db12d759c467a4e5382d9f
mendjoy <joycemendes.jm79@gmail.com>
2025-05-15 19:21:16 -0300

    controller e service para registrar a frequencia

--- src/main/java/io/github/mendjoy/gymJourneyAPI/controller/workout/WorkoutFrequencyController.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/controller/workout/WorkoutFrequencyController.java
@@ -1,4 +1,30 @@
 package io.github.mendjoy.gymJourneyAPI.controller.workout;
 
+import io.github.mendjoy.gymJourneyAPI.dto.responseAPI.ResponseApiDTO;
+import io.github.mendjoy.gymJourneyAPI.dto.workout.response.WorkoutFrequencyResponseDTO;
+import io.github.mendjoy.gymJourneyAPI.service.workout.WorkoutFrequencyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/workout/frequency")
 public class WorkoutFrequencyController {
+
+    private final WorkoutFrequencyService workoutFrequencyService;
+
+    public WorkoutFrequencyController(WorkoutFrequencyService workoutFrequencyService){
+        this.workoutFrequencyService = workoutFrequencyService;
+    }
+
+    @PostMapping("{idWorkout}/{date}")
+    public ResponseEntity<ResponseApiDTO> registerFrequency(@RequestParam Integer idWorkout, @RequestParam LocalDate date){
+        WorkoutFrequencyResponseDTO workoutFrequencyResponseDTO = workoutFrequencyService.registerFrequency(idWorkout, date);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseApiDTO.success(HttpStatus.CREATED, workoutFrequencyResponseDTO));
+    }
 }
--- src/main/java/io/github/mendjoy/gymJourneyAPI/service/workout/WorkoutFrequencyService.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/service/workout/WorkoutFrequencyService.java
@@ -1,4 +1,58 @@
 package io.github.mendjoy.gymJourneyAPI.service.workout;
 
+import io.github.mendjoy.gymJourneyAPI.dto.workout.response.WorkoutFrequencyResponseDTO;
+import io.github.mendjoy.gymJourneyAPI.entity.user.User;
+import io.github.mendjoy.gymJourneyAPI.entity.workout.Workout;
+import io.github.mendjoy.gymJourneyAPI.entity.workout.WorkoutFrequency;
+import io.github.mendjoy.gymJourneyAPI.exception.custom.CustomGymJourneyApiException;
+import io.github.mendjoy.gymJourneyAPI.repository.workout.WorkoutFrequencyRepository;
+import io.github.mendjoy.gymJourneyAPI.repository.workout.WorkoutRepository;
+import io.github.mendjoy.gymJourneyAPI.service.UserAuthService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Service
 public class WorkoutFrequencyService {
+
+    private final UserAuthService userAuthService;
+    private final WorkoutRepository workoutRepository;
+    private final WorkoutFrequencyRepository workoutFrequencyRepository;
+
+    public WorkoutFrequencyService(UserAuthService userAuthService, WorkoutRepository workoutRepository, WorkoutFrequencyRepository workoutFrequencyRepository) {
+        this.userAuthService = userAuthService;
+        this.workoutRepository = workoutRepository;
+        this.workoutFrequencyRepository = workoutFrequencyRepository;
+    }
+
+    public WorkoutFrequencyResponseDTO registerFrequency(Integer idWorkout, LocalDate date){
+        User user = userAuthService.getUserAuthenticate();
+        Workout currentWorkout = workoutRepository.findCurrentWorkout(user).orElseThrow(() -> new EntityNotFoundException("Não existe treino atual cadastrado para o usuario!"));
+        Workout workout = workoutRepository.findById(idWorkout).orElseThrow(() -> new EntityNotFoundException("Treino não cadastrado!"));
+
+        if(!Objects.equals(workout.getId(), currentWorkout.getId())){
+            throw new CustomGymJourneyApiException(HttpStatus.FORBIDDEN, "Frequencia só pode ser registrada para o treino atual!");
+        }
+
+        if (date == null) {
+            date = LocalDate.now();
+        }
+        if (date.isAfter(LocalDate.now())) {
+            throw new CustomGymJourneyApiException(HttpStatus.BAD_REQUEST, "Não é permitido registrar frequência futura!");
+        }
+
+        if(workoutFrequencyRepository.existsByUserAndWorkoutAndDate(user,workout,date)){
+            throw new CustomGymJourneyApiException(HttpStatus.CONFLICT,  String.format("Frequência já registrada para a data %s.", date));
+        }
+
+        WorkoutFrequency workoutFrequency = new WorkoutFrequency(user, workout, date);
+        WorkoutFrequency newWorkoutFrequency = workoutFrequencyRepository.save(workoutFrequency);
+        return new WorkoutFrequencyResponseDTO(newWorkoutFrequency.getId(),
+                                               newWorkoutFrequency.getUser().getId(),
+                                               newWorkoutFrequency.getWorkout().getId(),
+                                               newWorkoutFrequency.getFrequencyDate());
+    }
 }
