6b5d51be64987a4ce747ea37ce16dc4fc3a10fc6
mendjoy <joycemendes.jm79@gmail.com>
2025-05-15 19:07:29 -0300

    adcionando retorno opcional para workout

--- src/main/java/io/github/mendjoy/gymJourneyAPI/repository/workout/WorkoutRepository.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/repository/workout/WorkoutRepository.java
@@ -1,4 +1,4 @@
-package io.github.mendjoy.gymJourneyAPI.repository;
+package io.github.mendjoy.gymJourneyAPI.repository.workout;
 
 import io.github.mendjoy.gymJourneyAPI.entity.user.User;
 import io.github.mendjoy.gymJourneyAPI.entity.workout.Workout;
@@ -8,13 +8,14 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
 
     List<Workout> findByUser(User user);
 
     @Query("SELECT w FROM Workout w WHERE w.user = :user AND w.startDate <= CURRENT_DATE AND (w.endDate IS NULL OR w.endDate >= CURRENT_DATE)")
-    Workout findCurrentWorkout(@Param("user") User user);
+    Optional<Workout> findCurrentWorkout(@Param("user") User user);
 
     @Query("SELECT CASE WHEN COUNT(w) > 0 THEN true ELSE false END " +
     "FROM Workout w WHERE w.user = :user AND (w.endDate IS NULL OR w.endDate >= :newStartDate)")
