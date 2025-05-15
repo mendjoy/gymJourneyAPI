91436177d9be2e127f4cebe30cdbc57e9ae02336
mendjoy <joycemendes.jm79@gmail.com>
2025-05-15 19:06:34 -0300

    adicionando query methods para o repositorio de frequencia de treino

--- src/main/java/io/github/mendjoy/gymJourneyAPI/repository/workout/WorkoutFrequencyRepository.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/repository/workout/WorkoutFrequencyRepository.java
@@ -1,4 +1,13 @@
 package io.github.mendjoy.gymJourneyAPI.repository.workout;
 
-public interface WorkoutFrequencyRepository {
+import io.github.mendjoy.gymJourneyAPI.entity.user.User;
+import io.github.mendjoy.gymJourneyAPI.entity.workout.Workout;
+import io.github.mendjoy.gymJourneyAPI.entity.workout.WorkoutFrequency;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+
+public interface WorkoutFrequencyRepository extends JpaRepository<WorkoutFrequency, Integer> {
+
+    boolean existsByUserAndWorkoutAndDate(User user, Workout workout, LocalDate date);
 }
