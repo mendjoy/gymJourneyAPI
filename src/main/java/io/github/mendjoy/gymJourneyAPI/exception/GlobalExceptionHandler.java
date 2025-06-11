6b8a221c37d194eff5630e952e6fd6cc892b4237
mendjoy <joycemendes.jm79@gmail.com>
2025-06-10 22:01:48 -0300

    retornando msg de erro e bad request nas validacoes dos campos

--- src/main/java/io/github/mendjoy/gymJourneyAPI/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/exception/GlobalExceptionHandler.java
@@ -7,10 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.nio.file.AccessDeniedException;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,7 +26,11 @@ public class GlobalExceptionHandler {
             status = HttpStatus.NOT_FOUND;
         }else if(exception instanceof BadCredentialsException || exception instanceof AccessDeniedException){
             status = HttpStatus.UNAUTHORIZED;
-        }else {
+        }else if(exception instanceof MethodArgumentNotValidException){
+            message = getMessageValidator((MethodArgumentNotValidException) exception);
+            status = HttpStatus.BAD_REQUEST;
+        }
+        else {
             message = "Ocorreu um erro interno. Tente novamente mais tarde.";
         }
 
@@ -32,10 +38,16 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(responseApi, responseApi.getStatus());
     }
 
-
     @ExceptionHandler(CustomGymJourneyApiException.class)
     public ResponseEntity<ResponseApiDTO> handleCustomGymJourneyApiException(CustomGymJourneyApiException exception){
         ResponseApiDTO responseApi = ResponseApiDTO.error(exception.getStatusCode(), exception.getMessage());
         return new ResponseEntity<>(responseApi, responseApi.getStatus());
+    }
+
+    private String getMessageValidator(MethodArgumentNotValidException exception){
+        return exception.getBindingResult().getFieldErrors()
+                 .stream()
+                 .map(e -> e.getField() + ":" + e.getDefaultMessage())
+                 .collect(Collectors.joining(";"));
     }
 }
