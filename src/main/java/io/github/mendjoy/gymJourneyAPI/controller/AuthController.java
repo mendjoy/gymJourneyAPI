c0a23e29d0e711cfb06b6af2fb99dd9116109443
mendjoy <joycemendes.jm79@gmail.com>
2025-10-13 21:21:14 -0300

    controller e service para fazer autenticacao

--- src/main/java/io/github/mendjoy/gymJourneyAPI/controller/AuthController.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/controller/AuthController.java
@@ -1,4 +1,28 @@
 package io.github.mendjoy.gymJourneyAPI.controller;
 
+import io.github.mendjoy.gymJourneyAPI.dto.UserLoginDto;
+import io.github.mendjoy.gymJourneyAPI.dto.UserTokenDto;
+import io.github.mendjoy.gymJourneyAPI.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserTokenDto> login(@Valid @RequestBody UserLoginDto userLoginDto){
+        UserTokenDto userTokenDto = authService.login(userLoginDto);
+        return ResponseEntity.ok(userTokenDto);
+    }
 }
--- src/main/java/io/github/mendjoy/gymJourneyAPI/service/AuthService.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/service/AuthService.java
@@ -1,4 +1,40 @@
 package io.github.mendjoy.gymJourneyAPI.service;
 
+import io.github.mendjoy.gymJourneyAPI.domain.User;
+import io.github.mendjoy.gymJourneyAPI.dto.UserLoginDto;
+import io.github.mendjoy.gymJourneyAPI.dto.UserTokenDto;
+import io.github.mendjoy.gymJourneyAPI.exception.GymJourneyException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+
+    public AuthService(AuthenticationManager authenticationManager, TokenService tokenService) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+    }
+
+    public UserTokenDto login(UserLoginDto userLoginDto){
+
+        try {
+
+            UsernamePasswordAuthenticationToken tokenAuth = new UsernamePasswordAuthenticationToken(userLoginDto.email(), userLoginDto.password());
+            Authentication auth = authenticationManager.authenticate(tokenAuth);
+            String newToken = tokenService.generateToken((User) auth.getPrincipal());
+            return new UserTokenDto(newToken);
+
+        }catch (BadCredentialsException e){
+
+            throw GymJourneyException.forbidden("Email ou senha inválidos!");
+            
+        }
+    }
+
 }
