4559b795d30f3aa086bf09e1efc1c6fd7faf204a
mendjoy <joycemendes.jm79@gmail.com>
2025-11-03 10:54:00 -0300

    adicionando comandos para inserir resgitros na tabela de grupo musculates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,19 @@
-INSERT INTO role (name) VALUES ('ADMIN')   ON CONFLICT (name) DO NOTHING;
-INSERT INTO role (name) VALUES ('TRAINER') ON CONFLICT (name) DO NOTHING;
-INSERT INTO role (name) VALUES ('USER')    ON CONFLICT (name) DO NOTHING;
+INSERT INTO role (name) VALUES
+('ADMIN'),
+('TRAINER'),
+('USER')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO muscle_group (name) VALUES
+('Peito'),
+('Costas'),
+('Ombros'),
+('Bíceps'),
+('Tríceps'),
+('Antebraço'),
+('Abdômen'),
+('Quadríceps'),
+('Posterior de coxa'),
+('Glúteos'),
+('Panturrilhas')
+ON CONFLICT (name) DO NOTHING;
