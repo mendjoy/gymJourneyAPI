9ad89b08c061d2540ff7984580056856ecd649ed
mendjoy <joycemendes.jm79@gmail.com>
2026-01-25 11:22:34 -0300

    ajustes

--- src/main/java/io/github/mendjoy/gymJourneyAPI/domain/User.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/domain/User.java
@@ -36,7 +36,7 @@ public class User implements UserDetails {
     @Column(nullable = false, length = 255)
     private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
