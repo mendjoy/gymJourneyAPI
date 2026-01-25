0c2652e55f1833d585032dac99b1026a0717385e
mendjoy <joycemendes.jm79@gmail.com>
2026-01-25 11:37:16 -0300

    adiconando validacao

--- src/main/java/io/github/mendjoy/gymJourneyAPI/dto/user/UserDto.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/dto/user/UserDto.java
@@ -2,6 +2,8 @@ package io.github.mendjoy.gymJourneyAPI.dto.user;
 
 
 import io.github.mendjoy.gymJourneyAPI.dto.role.RoleDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,6 +11,9 @@ import java.util.List;
 public record UserDto(
         Long id,
         String email,
+
+        @NotBlank(message = "O nome é obrigatório")
+        @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
         String name,
         String phone,
         LocalDate birthDate,
