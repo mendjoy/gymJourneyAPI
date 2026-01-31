f4cee3d21144df02e5b1afb730e009147faa3532
mendjoy <joycemendes.jm79@gmail.com>
2026-01-31 10:00:00 -0300

    protegendo rotas de workout-section

--- src/main/java/io/github/mendjoy/gymJourneyAPI/config/security/SecurityConfig.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/config/security/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                             .requestMatchers(HttpMethod.POST, "/workouts").hasAnyRole("ADMIN", "TRAINER")
                             .requestMatchers(HttpMethod.DELETE, "/workouts").hasAnyRole("ADMIN", "TRAINER")
                             .requestMatchers(HttpMethod.PUT, "/workouts").hasAnyRole("ADMIN", "TRAINER")
+                            .requestMatchers("/workout-sections/**").hasAnyRole("ADMIN", "TRAINER")
                             .anyRequest().authenticated();
                 })
                 .build();
